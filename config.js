5c8f5f6302aa56a2c8ca53d14554c8f84c70fd64
Kewtii <86767429+Kewtii@users.noreply.github.com>
2021-07-01 14:23:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    introTitle: 'Babii aaa',
+    introDesc: `Em hỏi anh 1 câu nghiêm túc nhớ`,
+    btnIntro: 'Yeee babi',
+    title: 'Anh có iu em honggg',
+    desc: 'Iuu nhiều vãii hongg ',
+    btnYes: 'Iuu em nhiều nhất quả đất',
+    btnNo: 'Leuleu iu mỗi 1 tí',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại iu em nhìuu thế ?',
+    btnReply: 'Gửi cho bé iu',
+    reply: 'Tại vì em quá sức là đáng iêu',
+    mess: 'Hihi em biết màaa :"> Iu anhh',
+    messDesc: 'Chúc anh iu sớm được thịt iem leuleuu.',
+    btnAccept: 'Oki em nhóoo',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
